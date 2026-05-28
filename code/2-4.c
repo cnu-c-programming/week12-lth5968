1919aae22c65a6cc6e2acee875b74d08addd01ad
lth5968 <lth5968@gmail.com>
2026-05-28 22:30:05 +0900

    Update 2-4.c

--- code/2-4.c
+++ code/2-4.c
@@ -5,9 +5,23 @@ int main(int argc, const char* argv[]) {
         return 0;
 
     FILE* fp = fopen(argv[1], "r");
+    
+    if (!fp) return 0;
 
+    int counts[26] = {0};
+    int ch;
 
+    while ((ch = fgetc(fp)) != EOF) {
+        if (ch >= 'a' && ch <= 'z') {
+            counts[ch - 'a']++;
+        }
+    }
+
+    for (int i = 0; i < 26; i++) {
+        printf("%c: %d\n", 'a' + i, counts[i]);
+    }
 
     fclose(fp);
-}
 
+    return 0;
+}
