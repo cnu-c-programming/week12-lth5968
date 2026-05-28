1d3b6b88902cb5255ddac1792f308a1872aa2ece
lth5968 <lth5968@gmail.com>
2026-05-28 22:18:48 +0900

    Update 2-2.c

--- code/2-2.c
+++ code/2-2.c
@@ -11,12 +11,26 @@ int main(int argc, const char* argv[]) {
     int count = 0;
     Student students[64];
 
-
+    while (!feof(fp)) {
+        if (fscanf(fp, "%s %d", students[count].name, &students[count].score) == 2) {
+            count++;
+        }
+    }
 
     int max = 0;
     float avg = 0;
+    int sum = 0;
 
+    for (int i = 0; i < count; i++) {
+        if (students[i].score > max) {
+            max = students[i].score;
+        }
+        sum += students[i].score;
+    }
 
+    if (count > 0) {
+        avg = (float)sum / count;
+    }
 
     printf("max: %d\n", max);
     printf("avg: %.2f\n", avg);
@@ -25,4 +39,3 @@ int main(int argc, const char* argv[]) {
 
     return 0;
 }
-
