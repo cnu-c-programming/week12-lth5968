aa82259c7cb1b4d194a0503a2e80fe68ee1a6180
lth5968 <lth5968@gmail.com>
2026-05-28 22:15:20 +0900

    Update 2-1.c

--- code/2-1.c
+++ code/2-1.c
@@ -4,18 +4,17 @@
 int main(int argc, const char* argv[], const char* envp[]) {
     FILE* fp = fopen("student.txt", "w");
 
-    while(1) {
-        char name [10] = {0};
+    while (1) {
+        char name[10] = {0};
         int score = 0;
         scanf("%s", name);
-        if(strcmp(name, "quit") == 0)
+        if (strcmp(name, "quit") == 0)
             break;
-        scanf("%d", score);
-
+        scanf("%d", &score);
+        
         fprintf(fp, "%s %d\n", name, score);
     }
 
     fclose(fp);
     return 0;
 }
-
