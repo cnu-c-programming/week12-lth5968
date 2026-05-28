5863d90b34cb551814655d02a5452a7ebc828d5c
lth5968 <lth5968@gmail.com>
2026-05-28 23:18:04 +0900

    Update 3-5.c

--- code/3-5.c
+++ code/3-5.c
@@ -8,9 +8,17 @@ int main(int argc, const char* argv[]) {
         return 0;
     }
 
-
     int sum = 0;
+    char buf[64];
 
+    while (fscanf(fp, "%s", buf) == 1) {
+        int num;
+        if (sscanf(buf, "%d", &num) == 1) {
+            sum += num;
+        } else {
+            fprintf(stderr, "invalid input %s\n", buf);
+        }
+    }
 
     printf("sum: %d\n", sum);
     fclose(fp);
