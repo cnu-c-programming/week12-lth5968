227bf29635adbc82c6d708f63b0e8186a8ae706e
lth5968 <lth5968@gmail.com>
2026-05-28 22:44:03 +0900

    Update 2-6.c

--- code/2-6.c
+++ code/2-6.c
@@ -7,9 +7,16 @@ int main(int argc, const char* argv[]) {
 
     FILE* fp = fopen(argv[1], "r");
     const char* target_str = argv[2];
-
-
     
-    fclose(fp);
-}
+    char buffer[1024];
 
+    while (fgets(buffer, sizeof(buffer), fp) != NULL) {
+        if (strstr(buffer, target_str) != NULL) {
+            printf("%s", buffer);
+        }
+    }
+
+    fclose(fp);
+
+    return 0;
+}
